f6e9904852ebe3c5925e17ff170631c180fdae40
Santiago Torres <santiago_oziel92@hotmail.com>
2019-11-25 14:19:14 -0600

    added lots of stuf

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ setup(
   name = 'fixed-python-firebase',         # How you named your package folder (MyLib)
   packages = ['fixed-python-firebase'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='wtfpl',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Santiago Torres',                   # Type in your name
   author_email = 'santiago_oziel92@hotmail.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/santiagoziel/fixed-python-firebase/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['fixed', 'python', 'firebase'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'requests',
+          'urlparse'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: wtfpl License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
