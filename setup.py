425a814fd784532e0d55985c32d0291bdedd736d
Santiago Torres <santiago_oziel92@hotmail.com>
2019-11-25 14:32:48 -0600

    quite el pharse

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ setup(
   download_url = 'https://github.com/santiagoziel/fixed-python-firebase/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['fixed', 'python', 'firebase'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'requests',
-          'urlparse'
+          'requests'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
